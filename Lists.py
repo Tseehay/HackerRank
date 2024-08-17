284152076b96c557dbef543f33b0573be2b94a48
Tsehay Goremes <118212771+Tseehay@users.noreply.github.com>
2024-08-17 22:22:19 +0300

    list

---
+++ Lists.py
@@ -0,0 +1,23 @@
+if __name__ == '__main__':
+    N = int(input())
+    lst = []
+
+    for _ in range(N):
+        command = input().split()
+        
+        operation = command[0]
+        
+        if operation == "insert":
+            lst.insert(int(command[1]), int(command[2]))
+        elif operation == "print":
+            print(lst)
+        elif operation == "remove":
+            lst.remove(int(command[1]))
+        elif operation == "append":
+            lst.append(int(command[1]))
+        elif operation == "sort":
+            lst.sort()
+        elif operation == "pop":
+            lst.pop()
+        elif operation == "reverse":
+            lst.reverse()
